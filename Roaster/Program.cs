45f1533bb3c3286b184f7a76d49ec20f5ac20911
Catalin Modan <catalin.modan@softelligence.net>
2024-11-06 15:30:35 +0200

    feat: Add open telemetry support

--- Roaster/Program.cs
+++ Roaster/Program.cs
@@ -1,7 +1,37 @@
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Logging.AddOpenTelemetry(log =>
+{
+    log.IncludeScopes = true;
+    log.IncludeFormattedMessage = true;
+});
+builder.Services
+    .AddOpenTelemetry()
+    .WithMetrics(x => {
+       x.AddRuntimeInstrumentation();
+       x
+        .AddMeter("Microsoft.AspNetCore.Hosting")
+        .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
+        .AddMeter("System.Http")
+        .AddMeter("Roaster.Api.Roasts")
+        .AddPrometheusExporter();
+    })
+    .WithTracing(x =>
+    {
+        if(builder.Environment.IsDevelopment())
+        {
+            x.SetSampler<AlwaysOnSampler>();
+        }
+
+        x.AddAspNetCoreInstrumentation();
+        x.AddHttpClientInstrumentation();
+    });
+
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
@@ -13,4 +43,5 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+app.MapPrometheusScrapingEndpoint();
 app.Run();
