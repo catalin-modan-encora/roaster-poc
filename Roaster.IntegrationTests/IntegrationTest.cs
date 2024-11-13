d90de8f5a6c512f65d6f0145d9e5ad3cdee7304b
Catalin Modan <187375016+catalin-modan-encora@users.noreply.github.com>
2024-11-13 14:47:10 +0200

    feat: add github actions (#1)
    * feat: add github actions
    
    * feat: add dummy unit and integration tests
    
    * feat: migrate to local variables and add test stage
    
    * Rename test file
    
    * feat: add project refs
    
    * feat: run integration tests as well
    
    * chore(release): 1.1.0
    
    * ci: add initial docker
    
    * test: allow master PR for initial testing
    
    * chore: drop PR trigger on docker
    
    ---------
    
    Co-authored-by: Catalin Modan <catalin.modan@softelligence.net>

---
+++ Roaster.IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,12 @@
+namespace Roaster.IntegrationTests
+{
+    [TestClass]
+    public class IntegrationTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
