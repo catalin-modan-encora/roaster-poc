ae4b4e9fc6c250e65fecba1dff264aef2d7472ba
Catalin Modan <catalin.modan@softelligence.net>
2024-11-18 11:43:52 +0200

    fix: controller paths

--- Roaster/Controllers/RoastsController.cs
+++ Roaster/Controllers/RoastsController.cs
@@ -20,7 +20,7 @@ namespace Roaster.Controllers
             _context = context;
         }
 
-        [HttpPost("/", Name = "CreateRoast")]
+        [HttpPost(Name = "CreateRoast")]
         public async Task<IActionResult> CreateAsync(
             [FromBody] CreateRoastRequest request,
             CancellationToken cancellationToken = default
@@ -35,7 +35,7 @@ namespace Roaster.Controllers
             return Created($"/api/roast/{protectedId}", null);
         }
 
-        [HttpGet("/id", Name = "GetRoastById")]
+        [HttpGet("id", Name = "GetRoastById")]
         public async Task<IActionResult> GetByIdAsync([FromQuery] string id, CancellationToken cancellationToken = default)
         {
             var unprotectedKey = UnprotectId(id);
@@ -44,7 +44,7 @@ namespace Roaster.Controllers
             return roast is null ? NotFound() : Ok(new GetRoastByIdResponse(ProtectId(roast.Id), roast.Name));
         }
 
-        [HttpGet("/", Name = "GetAllRoasts")]
+        [HttpGet(Name = "GetAllRoasts")]
         public async Task<IActionResult> GetAllAsync(CancellationToken cancellationToken = default)
         {
             var roasts = await _context.Roasts.ToListAsync(cancellationToken);
